ea00eebf15bef4281711ec3e4e3d9690e909e70f
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-05-26 19:18:07 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
+  sum = num1 + num2
+  sum
 
 end
 
 def subtraction(num1, num2)
+  subtraction = num1 - num2
+  return subtraction
 
 end
 
 def division(num1, num2)
+  division = num1 / num2
+  division
 
 end
 
 def multiplication(num1, num2)
+  multiplication = num1 * num2
+  return multiplication
 
 end
 
 def modulo(num1, num2)
+  modulo = num1% num2
+  modulo
 
 end
 
 def square_root(num)
+  square_root = Math.sqrt(num)
+  square_root
 
 end
